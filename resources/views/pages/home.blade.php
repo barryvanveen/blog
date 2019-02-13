3566c5b4e5c49891ccb89686946b5f9afdedd5a0
Barry van Veen <barryvanveen@gmail.com>
2019-02-13 19:45:53 +0100

    Move some html around

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.base')
 
 @section('body')
-    @include('blocks.home.index')
+    <section>
+
+        <h1 id="home-header">Homepage - Barry van Veen</h1>
+        <p>Introduction of myself, maybe with a photo</p>
+
+        <h2>Articles</h2>
+        <p>Latest articles and link to the full blog</p>
+
+        <h2>Projects</h2>
+        <p>Recent projects and link to all projects</p>
+
+        <h2>Personal</h2>
+        <p>Music, books, other stuff</p>
+
+    </section>
 @endsection
