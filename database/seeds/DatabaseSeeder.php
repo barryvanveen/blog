d4405e8445152dc5d657c63471e7910b9fe2f93f
Barry van Veen <barryvanveen@gmail.com>
2019-02-13 19:45:18 +0100

    Seed user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 use App\Domain\Articles\Models\Article;
 use App\Domain\Authors\Models\Author;
+use App\Domain\Users\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        factory(User::class)->create([
+            'email' => 'admin@example.com',
+            'name' => 'Barry',
+        ]);
+
         /** @var Author $author */
         $author = factory(Author::class)->create();
 
