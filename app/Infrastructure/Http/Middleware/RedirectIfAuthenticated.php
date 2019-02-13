051c3863a6e36c19901c17cbc7e70d80442e801b
Barry van Veen <barryvanveen@gmail.com>
2019-02-13 19:44:48 +0100

    Fix middleware

--- app/Infrastructure/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Infrastructure/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect(route('admin.dashboard'));
         }
 
         return $next($request);
--- app/Infrastructure/Http/Middleware/VerifyCsrfToken.php
+++ app/Infrastructure/Http/Middleware/VerifyCsrfToken.php
@@ -20,6 +20,5 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array
      */
-    protected $except = [
-    ];
+    protected $except = [];
 }
