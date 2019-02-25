1421d8704a473e4c1b8523a2a391f0bd1dfeab9d
Barry van Veen <barryvanveen@gmail.com>
2019-02-25 21:26:08 +0100

    Enable AuthenticateSession web middleware

--- app/Infrastructure/Http/Kernel.php
+++ app/Infrastructure/Http/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends HttpKernel
             \App\Infrastructure\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Infrastructure\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
