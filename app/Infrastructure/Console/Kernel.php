c75044d8238ee651e6737f621904d99d0904c822
Barry van Veen <barryvanveen@gmail.com>
2019-02-25 21:25:11 +0100

    Remove unused console routes

--- app/Infrastructure/Console/Kernel.php
+++ app/Infrastructure/Console/Kernel.php
@@ -10,14 +10,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-    ];
-
-    /**
      * Define the application's command schedule.
      *
      * @param \Illuminate\Console\Scheduling\Schedule $schedule
@@ -32,7 +24,5 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
     }
 }
