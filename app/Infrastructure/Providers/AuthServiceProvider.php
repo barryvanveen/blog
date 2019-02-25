1e6a5771ffb8a6b124f8ad544634ce8f4e2ae48b
Barry van Veen <barryvanveen@gmail.com>
2019-02-25 21:02:41 +0100

    Add new bindings to service provider

--- app/Infrastructure/Providers/AuthServiceProvider.php
+++ app/Infrastructure/Providers/AuthServiceProvider.php
@@ -9,9 +9,15 @@ use App\Application\Auth\Commands\Logout;
 use App\Application\Auth\Handlers\LoginHandler;
 use App\Application\Auth\Handlers\LogoutHandler;
 use App\Application\Auth\Handlers\RateLimitedLoginHandler;
-use App\Application\Auth\LaravelRateLimiter;
-use App\Application\Auth\RateLimiterInterface;
 use App\Application\Core\CommandBusInterface;
+use App\Application\Interfaces\GuardInterface;
+use App\Application\Interfaces\RateLimiterInterface;
+use App\Application\Interfaces\SessionInterface;
+use App\Application\Interfaces\TranslatorInterface;
+use App\Infrastructure\Adapters\LaravelGuard;
+use App\Infrastructure\Adapters\LaravelRateLimiter;
+use App\Infrastructure\Adapters\LaravelSession;
+use App\Infrastructure\Adapters\LaravelTranslator;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -22,7 +28,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(CommandBusInterface $commandBus)
     {
+        $this->app->bind(GuardInterface::class, LaravelGuard::class);
+        $this->app->bind(SessionInterface::class, LaravelSession::class);
         $this->app->bind(RateLimiterInterface::class, LaravelRateLimiter::class);
+        $this->app->bind(TranslatorInterface::class, LaravelTranslator::class);
 
         $this->app->bind(RateLimitedLoginHandler::class, function (Application $app) {
             return new RateLimitedLoginHandler(
