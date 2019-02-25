dca7bacddd438c5a3fc83441b0522235f4353e2f
Barry van Veen <barryvanveen@gmail.com>
2019-02-25 21:27:56 +0100

    Remove unused traits

--- app/Infrastructure/Http/Controllers/Controller.php
+++ app/Infrastructure/Http/Controllers/Controller.php
@@ -4,13 +4,8 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    // todo: are these traits really needed?
 }
