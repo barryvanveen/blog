58bfcfb626c326f68906e9b48af3e94a96659764
Barry van Veen <barryvanveen@gmail.com>
2019-10-02 21:34:23 +0200

    Add base url to all pages

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="csrf-token" content="{{ $csrf_token }}">
+        <base href="{{ $base_url }}">
 
         <title>Home - A blog about web development - Barry van Veen</title>
 
