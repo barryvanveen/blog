8869c049dcf87a50d600c4c2c13a1546696df139
Barry van Veen <barryvanveen@gmail.com>
2019-10-02 16:44:11 +0200

    Remove unnecessary templates

--- resources/views/pages/articles/index.blade.php
+++ resources/views/pages/articles/index.blade.php
@@ -1,5 +1,13 @@
+@presenter(App\Application\Articles\View\ArticlesIndexPresenter)
+
 @extends('layouts.base')
 
 @section('body')
-    @include('blocks.articles.index')
+    <section>
+        <h1 id="title">Articles</h1>
+
+        <ul>
+            @each('blocks.articles.partials.card', $articles, 'article')
+        </ul>
+    </section>
 @endsection
