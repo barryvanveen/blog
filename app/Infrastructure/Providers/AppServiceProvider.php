40baf51800ca4b9b176ab45f0b46184611a00464
Barry van Veen <barryvanveen@gmail.com>
2019-10-02 15:02:47 +0200

    Register new bindings in container

--- app/Infrastructure/Providers/AppServiceProvider.php
+++ app/Infrastructure/Providers/AppServiceProvider.php
@@ -8,7 +8,9 @@ use App\Application\Core\CommandBusInterface;
 use App\Application\Core\ResponseBuilder;
 use App\Application\Core\ResponseBuilderInterface;
 use App\Application\Core\UniqueIdGenerator;
+use App\Application\Interfaces\ConfigurationInterface;
 use App\Application\Interfaces\GuardInterface;
+use App\Application\Interfaces\PathBuilderInterface;
 use App\Application\Interfaces\QueryBuilderInterface;
 use App\Application\Interfaces\RateLimiterInterface;
 use App\Application\Interfaces\SessionInterface;
@@ -16,8 +18,12 @@ use App\Application\Interfaces\SlugFactoryInterface;
 use App\Application\Interfaces\TranslatorInterface;
 use App\Application\Interfaces\UrlGeneratorInterface;
 use App\Application\Interfaces\ViewBuilderInterface;
+use App\Application\View\AssetUrlBuilder;
+use App\Application\View\AssetUrlBuilderInterface;
 use App\Domain\Core\UniqueIdGeneratorInterface;
+use App\Infrastructure\Adapters\LaravelConfiguration;
 use App\Infrastructure\Adapters\LaravelGuard;
+use App\Infrastructure\Adapters\LaravelPathBuilder;
 use App\Infrastructure\Adapters\LaravelQueryBuilder;
 use App\Infrastructure\Adapters\LaravelRateLimiter;
 use App\Infrastructure\Adapters\LaravelSession;
@@ -32,17 +38,20 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->app->bind(AssetUrlBuilderInterface::class, AssetUrlBuilder::class);
+        $this->app->singleton(CommandBusInterface::class, LaravelCommandBus::class);
+
+        $this->app->bind(ConfigurationInterface::class, LaravelConfiguration::class);
         $this->app->bind(GuardInterface::class, LaravelGuard::class);
+        $this->app->bind(PathBuilderInterface::class, LaravelPathBuilder::class);
+        $this->app->bind(QueryBuilderInterface::class, LaravelQueryBuilder::class);
         $this->app->bind(RateLimiterInterface::class, LaravelRateLimiter::class);
         $this->app->bind(ResponseBuilderInterface::class, ResponseBuilder::class);
-        $this->app->bind(QueryBuilderInterface::class, LaravelQueryBuilder::class);
         $this->app->bind(SessionInterface::class, LaravelSession::class);
         $this->app->bind(SlugFactoryInterface::class, LaravelSlugFactory::class);
         $this->app->bind(TranslatorInterface::class, LaravelTranslator::class);
         $this->app->bind(UniqueIdGeneratorInterface::class, UniqueIdGenerator::class);
         $this->app->bind(UrlGeneratorInterface::class, LaravelUrlGenerator::class);
         $this->app->bind(ViewBuilderInterface::class, LaravelViewBuilder::class);
-
-        $this->app->singleton(CommandBusInterface::class, LaravelCommandBus::class);
     }
 }
