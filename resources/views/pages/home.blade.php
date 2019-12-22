32a216928ae6cf11dd15d0e0e127c19404e61ac2
Barry van Veen <barryvanveen@gmail.com>
2019-12-22 17:35:04 +0100

    Home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,17 +3,36 @@
 @section('body')
     <section>
 
-        <h1 id="home-header">Homepage - Barry van Veen</h1>
-        <p>Introduction of myself, maybe with a photo</p>
+        <h1>Hi!</h1>
+
+        <p>Good to see you! This is my blog where I write about software development. Besides that you can read some more about who I am and what I do. Below is an overview of the latest updates.</p>
 
         <h2>Articles</h2>
-        <p>Latest articles and link to the full blog</p>
 
-        <h2>Projects</h2>
-        <p>Recent projects and link to all projects</p>
+        <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+        </ul>
 
-        <h2>Personal</h2>
-        <p>Music, books, other stuff</p>
+        <h2><a href="about/now">Now</a></h2>
+
+        <p>Setting up a development plan carousel at Coolblue. Doing lots of endurance training.</p>
+
+        <h2><a href="about/books">Books</a></h2>
+
+        <ul>
+            <li>DDD book</li>
+            <li>Moedervlekken by Arnon Grunberg</li>
+        </ul>
+
+        <h2>Music</h2>
+
+        <ul>
+            <li>Artist #1</li>
+            <li>Artist #2</li>
+            <li>Artist #3</li>
+        </ul>
 
     </section>
 @endsection
