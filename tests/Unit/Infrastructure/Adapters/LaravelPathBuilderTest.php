307793ebed376ba788818e7a82bd274730b114ce
Barry van Veen <barryvanveen@gmail.com>
2020-01-05 23:16:03 +0100

    Test LaravelPathBuilder

--- tests/Unit/Infrastructure/Adapters/LaravelPathBuilderTest.php
+++ tests/Unit/Infrastructure/Adapters/LaravelPathBuilderTest.php
@@ -65,4 +65,106 @@ class LaravelPathBuilderTest extends TestCase
             ],
         ];
     }
+
+    /**
+     * @test
+     *
+     * @dataProvider assetPathDataProvider
+     */
+    public function itExtendsTheAssetPath($postfix, $expected): void
+    {
+        /** @var ObjectProphecy|Application $application */
+        $application = $this->prophesize(Application::class);
+
+        $application
+            ->make(Argument::type('string'))
+            ->willReturn('/my/public/path');
+
+        $laravelPathBuilder = new LaravelPathBuilder(
+            $application->reveal()
+        );
+
+        $this->assertEquals($expected, $laravelPathBuilder->assetPath($postfix));
+    }
+
+    public function assetPathDataProvider()
+    {
+        return [
+            [
+                'manifest.json',
+                '/my/public/path/dist/manifest.json',
+            ],
+            [
+                '/manifest.json',
+                '/my/public/path/dist/manifest.json',
+            ],
+            [
+                'dist/manifest.json',
+                '/my/public/path/dist/dist/manifest.json',
+            ],
+            [
+                '/dist/manifest.json',
+                '/my/public/path/dist/dist/manifest.json',
+            ],
+            [
+                '../dist/manifest.json',
+                '/my/public/path/dist/../dist/manifest.json',
+            ],
+            [
+                '/../dist/manifest.json',
+                '/my/public/path/dist/../dist/manifest.json',
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider storagePathDataProvider
+     */
+    public function itExtendsTheStoragePath($postfix, $expected): void
+    {
+        /** @var ObjectProphecy|Application $application */
+        $application = $this->prophesize(Application::class);
+
+        $application
+            ->make(Argument::type('string'))
+            ->willReturn('/my/storage/path');
+
+        $laravelPathBuilder = new LaravelPathBuilder(
+            $application->reveal()
+        );
+
+        $this->assertEquals($expected, $laravelPathBuilder->storagePath($postfix));
+    }
+
+    public function storagePathDataProvider()
+    {
+        return [
+            [
+                'manifest.json',
+                '/my/storage/path/manifest.json',
+            ],
+            [
+                '/manifest.json',
+                '/my/storage/path/manifest.json',
+            ],
+            [
+                'dist/manifest.json',
+                '/my/storage/path/dist/manifest.json',
+            ],
+            [
+                '/dist/manifest.json',
+                '/my/storage/path/dist/manifest.json',
+            ],
+            [
+                '../dist/manifest.json',
+                '/my/storage/path/../dist/manifest.json',
+            ],
+            [
+                '/../dist/manifest.json',
+                '/my/storage/path/../dist/manifest.json',
+            ],
+        ];
+    }
 }
