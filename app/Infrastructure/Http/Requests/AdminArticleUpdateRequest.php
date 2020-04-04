c457f8a4116b624f5ba4e5f333afc77ab6018047
Barry van Veen <barryvanveen@gmail.com>
2020-04-04 12:36:33 +0200

    Remove duplicate code

--- app/Infrastructure/Http/Requests/AdminArticleUpdateRequest.php
+++ app/Infrastructure/Http/Requests/AdminArticleUpdateRequest.php
@@ -7,45 +7,10 @@ namespace App\Infrastructure\Http\Requests;
 use App\Domain\Articles\Requests\AdminArticleUpdateRequestInterface;
 
 /** @psalm-suppress PropertyNotSetInConstructor */
-class AdminArticleUpdateRequest extends BaseRequest implements AdminArticleUpdateRequestInterface
+class AdminArticleUpdateRequest extends AdminArticleCreateRequest implements AdminArticleUpdateRequestInterface
 {
-    public function rules(): array
-    {
-        return [
-            'title' => 'required|string',
-            'published_at' => 'required|date_format:Y-m-d H:i:s',
-            'description' => 'required|string',
-            'content' => 'required|string',
-        ];
-    }
-
     public function uuid(): string
     {
         return $this->getRouteParameterAsString('uuid');
-    }
-
-    public function title(): string
-    {
-        return $this->getInputParameterAsString('title');
-    }
-
-    public function publishedAt(): string
-    {
-        return $this->getInputParameterAsString('published_at');
-    }
-
-    public function description(): string
-    {
-        return $this->getInputParameterAsString('description');
-    }
-
-    public function content(): string
-    {
-        return $this->getInputParameterAsString('content');
-    }
-
-    public function status(): int
-    {
-        return $this->getInputParameterAsInteger('status');
     }
 }
