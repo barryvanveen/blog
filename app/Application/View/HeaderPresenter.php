052b32264489d7279a66714ae0706f0e620bb37b
Barry van Veen <barryvanveen@gmail.com>
2020-04-12 22:37:43 +0200

    Present admin menu when in admin section

--- app/Application/View/HeaderPresenter.php
+++ app/Application/View/HeaderPresenter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Application\View;
 
+use App\Application\Interfaces\RouterInterface;
 use App\Application\Interfaces\UrlGeneratorInterface;
 use App\Domain\Utils\MenuItem;
 
@@ -12,17 +13,39 @@ final class HeaderPresenter implements PresenterInterface
     /** @var UrlGeneratorInterface */
     private $urlGenerator;
 
+    /** @var RouterInterface */
+    private $router;
+
     public function __construct(
-        UrlGeneratorInterface $urlGenerator
+        UrlGeneratorInterface $urlGenerator,
+        RouterInterface $router
     ) {
         $this->urlGenerator = $urlGenerator;
+        $this->router = $router;
     }
 
     public function present(): array
     {
+        if ($this->router->currentRouteIsAdminRoute()) {
+            return [
+                'home_url' => $this->urlGenerator->route('admin.dashboard'),
+                'menu_items' => $this->getAdminMenuItems(),
+            ];
+        }
+
         return [
             'home_url' => $this->urlGenerator->route('home'),
             'menu_items' => $this->getMenuItems(),
+        ];
+    }
+
+    /**
+     * @return \App\Domain\Utils\MenuItem[]
+     */
+    private function getAdminMenuItems(): array
+    {
+        return [
+            new MenuItem('Articles', $this->urlGenerator->route('admin.articles.index')),
         ];
     }
 
--- tests/Unit/Application/View/HeaderPresenterTest.php
+++ tests/Unit/Application/View/HeaderPresenterTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Application\View;
 
+use App\Application\Interfaces\RouterInterface;
 use App\Application\Interfaces\UrlGeneratorInterface;
 use App\Application\View\HeaderPresenter;
 use App\Domain\Utils\MenuItem;
@@ -17,14 +18,49 @@ use Tests\TestCase;
 class HeaderPresenterTest extends TestCase
 {
     /** @test */
-    public function itPassesTheCorrectVariables(): void
+    public function itPresentsTheMenuForVisitors(): void
     {
         /** @var ObjectProphecy|UrlGeneratorInterface $urlGenerator */
         $urlGenerator = $this->prophesize(UrlGeneratorInterface::class);
+        $urlGenerator->route(Argument::exact('home'))->willReturn('/homeroute')
+            ->shouldBeCalled();
         $urlGenerator->route(Argument::type('string'))->willReturn('/myroute');
 
+        /** @var ObjectProphecy|RouterInterface $router */
+        $router = $this->prophesize(RouterInterface::class);
+        $router->currentRouteIsAdminRoute()->willReturn(false)->shouldBeCalled();
+
         $presenter = new HeaderPresenter(
-            $urlGenerator->reveal()
+            $urlGenerator->reveal(),
+            $router->reveal()
+        );
+
+        $values = $presenter->present();
+
+        $this->assertArrayHasKey('home_url', $values);
+
+        $this->assertArrayHasKey('menu_items', $values);
+        foreach ($values['menu_items'] as $item) {
+            $this->assertInstanceOf(MenuItem::class, $item);
+        }
+    }
+
+    /** @test */
+    public function itPresentsTheMenuForTheAdminSection(): void
+    {
+        /** @var ObjectProphecy|UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $this->prophesize(UrlGeneratorInterface::class);
+        $urlGenerator->route(Argument::exact('admin.dashboard'))->willReturn('/homeroute')
+            ->shouldBeCalled();
+        $urlGenerator->route(Argument::type('string'))->willReturn('/myroute');
+
+        /** @var ObjectProphecy|RouterInterface $router */
+        $router = $this->prophesize(RouterInterface::class);
+        $router->currentRouteIsAdminRoute()->willReturn(true)->shouldBeCalled();
+
+        $presenter = new HeaderPresenter(
+            $urlGenerator->reveal(),
+            $router->reveal()
         );
 
         $values = $presenter->present();
