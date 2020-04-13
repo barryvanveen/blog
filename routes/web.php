8ae394d8b945875ccd1b8bcaa40acf53f3c8a585
Barry van Veen <barryvanveen@gmail.com>
2020-04-13 18:39:10 +0200

    Move HomeController to Application folder

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Infrastructure\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [\App\Application\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/about', [\App\Application\Http\Controllers\AboutController::class, 'index'])->name('about');
 Route::get('/articles', [\App\Infrastructure\Http\Controllers\ArticlesController::class, 'index'])->name('articles.index');
 Route::get('/articles/rss', [\App\Application\Http\Controllers\ArticlesRssController::class, 'index'])->name('articles.rss');
