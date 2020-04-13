f0eb17949836e85594962f685c84026324738700
Barry van Veen <barryvanveen@gmail.com>
2020-04-13 22:27:37 +0200

    Add redirect methods to ResponseBuilder

--- app/Application/Core/ResponseBuilder.php
+++ app/Application/Core/ResponseBuilder.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Application\Core;
 
+use App\Application\Interfaces\SessionInterface;
 use App\Application\Interfaces\UrlGeneratorInterface;
 use App\Application\Interfaces\ViewBuilderInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 
 class ResponseBuilder implements ResponseBuilderInterface
@@ -24,16 +26,26 @@ class ResponseBuilder implements ResponseBuilderInterface
     /** @var UrlGeneratorInterface */
     private $urlGenerator;
 
+    /** @var ServerRequestInterface */
+    private $request;
+
+    /** @var SessionInterface */
+    private $session;
+
     public function __construct(
         ViewBuilderInterface $viewBuilder,
         ResponseFactoryInterface $responseFactory,
         StreamFactoryInterface $streamFactory,
-        UrlGeneratorInterface $urlGenerator
+        UrlGeneratorInterface $urlGenerator,
+        ServerRequestInterface $request,
+        SessionInterface $session
     ) {
         $this->viewBuilder = $viewBuilder;
         $this->responseFactory = $responseFactory;
         $this->streamFactory = $streamFactory;
         $this->urlGenerator = $urlGenerator;
+        $this->request = $request;
+        $this->session = $session;
     }
 
     public function ok(string $view, array $data = []): ResponseInterface
@@ -54,6 +66,22 @@ class ResponseBuilder implements ResponseBuilderInterface
         return $response->withHeader('Location', $location);
     }
 
+    public function redirectBack(int $status): ResponseInterface
+    {
+        $location = $this->determinePreviousUrl();
+
+        return $this->responseFactory->createResponse($status)
+            ->withHeader('Location', $location);
+    }
+
+    public function redirectIntended(int $status, string $fallbackRoute): ResponseInterface
+    {
+        $location = $this->session->intendedUrl() ?? $this->urlGenerator->route($fallbackRoute);
+
+        return $this->responseFactory->createResponse($status)
+            ->withHeader('Location', $location);
+    }
+
     public function xml(string $view): ResponseInterface
     {
         $view = $this->viewBuilder->render($view);
@@ -63,5 +91,14 @@ class ResponseBuilder implements ResponseBuilderInterface
         return $this->responseFactory->createResponse(200)
             ->withBody($body)
             ->withHeader('Content-Type', 'text/xml');
+    }
+
+    private function determinePreviousUrl(): string
+    {
+        $referrer = $this->request->hasHeader('referer') ? $this->request->getHeader('referer')[0] : null;
+
+        $url = $referrer ?? $this->session->previousUrl();
+
+        return $url ?? $this->urlGenerator->route('home');
     }
 }
--- tests/Unit/Application/Core/ResponseBuilderTest.php
+++ tests/Unit/Application/Core/ResponseBuilderTest.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace Tests\Unit\Application\Core;
 
 use App\Application\Core\ResponseBuilder;
+use App\Application\Interfaces\SessionInterface;
 use App\Application\Interfaces\UrlGeneratorInterface;
 use App\Application\Interfaces\ViewBuilderInterface;
 use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7\ServerRequest;
 use Prophecy\Argument;
 use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Tests\TestCase;
 
@@ -23,17 +26,23 @@ class ResponseBuilderTest extends TestCase
     /** @var ObjectProphecy|ViewBuilderInterface */
     private $viewBuilder;
 
-    /** @var ObjectProphecy|ResponseFactoryInterface */
+    /** @var ResponseFactoryInterface */
     private $responseFactory;
 
-    /** @var ObjectProphecy|StreamFactoryInterface */
+    /** @var StreamFactoryInterface */
     private $streamFactory;
-
-    /** @var ResponseBuilder */
-    private $responseBuilder;
 
     /** @var ObjectProphecy|UrlGeneratorInterface */
     private $urlGenerator;
+
+    /** @var ServerRequestInterface */
+    private $request;
+
+    /** @var ObjectProphecy|SessionInterface */
+    private $session;
+
+    /** @var ResponseBuilder */
+    private $responseBuilder;
 
     public function setUp(): void
     {
@@ -43,12 +52,16 @@ class ResponseBuilderTest extends TestCase
         $this->responseFactory = new Psr17Factory();
         $this->streamFactory = new Psr17Factory();
         $this->urlGenerator = $this->prophesize(UrlGeneratorInterface::class);
+        $this->request = new ServerRequest('GET', '/');
+        $this->session = $this->prophesize(SessionInterface::class);
 
         $this->responseBuilder = new ResponseBuilder(
             $this->viewBuilder->reveal(),
             $this->responseFactory,
             $this->streamFactory,
-            $this->urlGenerator->reveal()
+            $this->urlGenerator->reveal(),
+            $this->request,
+            $this->session->reveal()
         );
     }
 
@@ -85,6 +98,113 @@ class ResponseBuilderTest extends TestCase
         $headers = $response->getHeaders();
         $this->assertArrayHasKey('Location', $headers);
         $this->assertEquals('fooUrl', $headers['Location'][0]);
+    }
+
+    /** @test */
+    public function itRedirectsBackToTheReferer(): void
+    {
+        // arrange
+        $request = $this->request->withHeader('referer', 'http://referer.url');
+        $this->session->previousUrl()->willReturn('http://session.url');
+        $this->urlGenerator->route(Argument::any())->willReturn('http://fallback.url');
+
+        $this->responseBuilder = new ResponseBuilder(
+            $this->viewBuilder->reveal(),
+            $this->responseFactory,
+            $this->streamFactory,
+            $this->urlGenerator->reveal(),
+            $request,
+            $this->session->reveal()
+        );
+
+        // act
+        $response = $this->responseBuilder->redirectBack(302);
+
+        // assert
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals(302, $response->getStatusCode());
+
+        $headers = $response->getHeaders();
+        $this->assertArrayHasKey('Location', $headers);
+        $this->assertEquals('http://referer.url', $headers['Location'][0]);
+    }
+
+    /** @test */
+    public function itRedirectsBackToThePreviousUrl(): void
+    {
+        // arrange
+        // no referer
+        $this->session->previousUrl()->willReturn('http://session.url');
+        $this->urlGenerator->route(Argument::any())->willReturn('http://fallback.url');
+
+        // act
+        $response = $this->responseBuilder->redirectBack(302);
+
+        // assert
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals(302, $response->getStatusCode());
+
+        $headers = $response->getHeaders();
+        $this->assertArrayHasKey('Location', $headers);
+        $this->assertEquals('http://session.url', $headers['Location'][0]);
+    }
+
+    /** @test */
+    public function itRedirectsBackToTheFallbackUrl(): void
+    {
+        // arrange
+        // no referer
+        // no previous url in session
+        $this->urlGenerator->route(Argument::any())->willReturn('http://fallback.url');
+
+        // act
+        $response = $this->responseBuilder->redirectBack(302);
+
+        // assert
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals(302, $response->getStatusCode());
+
+        $headers = $response->getHeaders();
+        $this->assertArrayHasKey('Location', $headers);
+        $this->assertEquals('http://fallback.url', $headers['Location'][0]);
+    }
+
+    /** @test */
+    public function itRedirectsToIntendedUrl(): void
+    {
+        // arrange
+        $this->session->intendedUrl()->willReturn('http://intended.url');
+        $this->urlGenerator->route(Argument::any())->willReturn('http://fallback.url');
+
+        // act
+        $response = $this->responseBuilder->redirectIntended(302, 'any');
+
+        // assert
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals(302, $response->getStatusCode());
+
+        $headers = $response->getHeaders();
+        $this->assertArrayHasKey('Location', $headers);
+        $this->assertEquals('http://intended.url', $headers['Location'][0]);
+    }
+
+    /** @test */
+    public function itRedirectsToFallbackUrl(): void
+    {
+        // arrange
+        // no intended url
+        $this->urlGenerator->route(Argument::any())->willReturn('http://fallback.url');
+
+        // act
+        $response = $this->responseBuilder->redirectIntended(302, 'any');
+
+        // assert
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals(302, $response->getStatusCode());
+
+        $headers = $response->getHeaders();
+        $this->assertArrayHasKey('Location', $headers);
+        $this->assertEquals('http://fallback.url', $headers['Location'][0]);
     }
 
     /** @test */
