f3c2d48a1ce8ed70d62ef473724e2ba9c5bda3b7
Barry van Veen <barryvanveen@gmail.com>
2020-04-14 21:32:50 +0200

    Add feature test for article item page

---
+++ tests/Feature/ArticleItemTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Infrastructure\Eloquent\ArticleEloquentModel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ArticleItemTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function seePublishedArticle(): void
+    {
+        /** @var ArticleEloquentModel $article */
+        $article = factory(ArticleEloquentModel::class)->states(['published', 'published_in_past'])->create([
+            'title' => 'FooArticle',
+        ]);
+
+        $response = $this->get(route('articles.show', ['uuid' => $article->uuid, 'slug' => $article->slug]));
+
+        $response->assertOk();
+        $response->assertSee($article->title);
+    }
+
+    /** @test */
+    public function unpublishedArticleResultsIn404ErrorPage(): void
+    {
+        /** @var ArticleEloquentModel $article */
+        $article = factory(ArticleEloquentModel::class)->states(['published', 'published_in_future'])->create([
+            'title' => 'FooArticle',
+        ]);
+
+        $response = $this->get(route('articles.show', ['uuid' => $article->uuid, 'slug' => $article->slug]));
+
+        $response->assertStatus(404);
+        $response->assertSee('Not Found');
+    }
+
+    /** @test */
+    public function itRedirectsToTheCorrectSlug(): void
+    {
+        /** @var ArticleEloquentModel $article */
+        $article = factory(ArticleEloquentModel::class)->states(['published', 'published_in_past'])->create([
+            'uuid' => 'myuuid',
+            'slug' => 'my-slug-string',
+        ]);
+
+        $response = $this->get(route('articles.show', ['uuid' => $article->uuid, 'slug' => 'wrong-slug']));
+
+        $response->assertStatus(301);
+        $response->assertRedirect(route('articles.show', ['uuid' => $article->uuid, 'slug' => $article->slug]));
+    }
+}
