aeac61f9f7679451272e5c53940f6736457cbe01
Barry van Veen <barryvanveen@gmail.com>
2020-07-12 18:14:30 +0200

    Configure remote database

--- config/database.php
+++ config/database.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Str;
-
 return [
     /*
     |--------------------------------------------------------------------------
@@ -52,6 +50,26 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        'remote_mysql' => [
+            'driver' => 'mysql',
+            'url' => env('REMOTE_DATABASE_URL'),
+            'host' => env('REMOTE_DB_HOST', '127.0.0.1'),
+            'port' => env('REMOTE_DB_PORT', '3306'),
+            'database' => env('REMOTE_DB_DATABASE', 'forge'),
+            'username' => env('REMOTE_DB_USERNAME', 'forge'),
+            'password' => env('REMOTE_DB_PASSWORD', ''),
+            'unix_socket' => env('REMOTE_DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
