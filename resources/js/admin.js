d1c68b8b5204a5fdc59254d0af45b64ce3987145
Barry van Veen <barryvanveen@gmail.com>
2020-09-18 20:27:19 +0200

    Add editor preview

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,5 +1,7 @@
 import editor from './admin/editor'
+import preview from './admin/preview'
 
 const styles = require('../sass/admin.pcss'); // eslint-disable-line
 
 editor()
+preview()
---
+++ resources/js/admin/preview.js
@@ -0,0 +1,67 @@
+import debounce from '../util/debounce'
+import getVariable from '../util/variables'
+
+const initPreviews = () => {
+  const editors = document.querySelectorAll('[data-editor]')
+
+  editors.forEach(editor => {
+    registerInputEventListener(editor)
+  })
+}
+
+const registerInputEventListener = (el) => {
+  const previewEl = document.querySelector(el.dataset.preview)
+
+  if (!previewEl) {
+    return
+  }
+
+  el.addEventListener('input', debouncedInputChangedListener)
+  el.dispatchEvent(new Event('input'))
+}
+
+const getHtmlFromMarkdown = (markdown) => {
+  const data = {
+    markdown: markdown
+  }
+
+  const url = getVariable('markdown_to_html_url')
+
+  return fetch(url, {
+    method: 'POST',
+    mode: 'same-origin',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content
+    },
+    body: JSON.stringify(data)
+  })
+}
+
+const setHtmlOnPreviewElement = (html, el) => {
+  const previewEl = document.querySelector(el.dataset.preview)
+
+  if (!previewEl) {
+    return
+  }
+
+  previewEl.innerHTML = html
+}
+
+const updatePreview = (event) => {
+  const markdown = event.target.value
+  getHtmlFromMarkdown(markdown)
+    .then(response => response.json())
+    .then(data => setHtmlOnPreviewElement(data.html, event.target))
+    .catch(err => console.warn('Something went wrong.', err))
+}
+
+const debouncedInputChangedListener = (event) => debounce(updatePreview(event), 500)
+
+const preview = () => {
+  initPreviews()
+}
+
+export default preview
