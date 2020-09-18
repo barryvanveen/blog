ba1ad1c0256e52fefcfea29b44d4521982e41dee
Barry van Veen <barryvanveen@gmail.com>
2020-09-18 20:46:11 +0200

    Fix form field labels

--- resources/views/partials/input/text.blade.php
+++ resources/views/partials/input/text.blade.php
@@ -1,8 +1,9 @@
 <div class="mb-4">
-    <label class="block font-bold mb-2" for="{{ $name }}">{{ $title }}</label>
+    <label class="block font-bold mb-2" for="text-{{ $name }}">{{ $title }}</label>
 
     <input class="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
            name="{{ $name }}"
+           id="text-{{ $name }}"
            type="text"
            placeholder="{{ $placeholder }}"
            value="{{ $value }}"
