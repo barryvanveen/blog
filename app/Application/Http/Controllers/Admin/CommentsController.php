13a745eb933b1e2e9441eaf95a2521f9da0e3856
Barry van Veen <barryvanveen@gmail.com>
2020-10-04 18:15:26 +0200

    Store new comments when submitting form

--- app/Application/Http/Controllers/Admin/CommentsController.php
+++ app/Application/Http/Controllers/Admin/CommentsController.php
@@ -4,21 +4,51 @@ declare(strict_types=1);
 
 namespace App\Application\Http\Controllers\Admin;
 
+use App\Application\Comments\Commands\CreateComment;
 use App\Application\Core\ResponseBuilderInterface;
+use App\Application\Interfaces\CommandBusInterface;
+use App\Domain\Comments\CommentStatus;
+use App\Domain\Comments\Requests\AdminCommentCreateRequestInterface;
+use DateTimeImmutable;
 use Psr\Http\Message\ResponseInterface;
 
 final class CommentsController
 {
     private ResponseBuilderInterface $responseBuilder;
+    private CommandBusInterface $commandBus;
 
     public function __construct(
-        ResponseBuilderInterface $responseBuilder
+        ResponseBuilderInterface $responseBuilder,
+        CommandBusInterface $commandBus
     ) {
         $this->responseBuilder = $responseBuilder;
+        $this->commandBus = $commandBus;
     }
 
     public function index(): ResponseInterface
     {
         return $this->responseBuilder->ok('comments.admin.index');
+    }
+
+    public function create(): ResponseInterface
+    {
+        return $this->responseBuilder->ok('comments.admin.create');
+    }
+
+    public function store(AdminCommentCreateRequestInterface $request): ResponseInterface
+    {
+        $command = new CreateComment(
+            $request->articleUuid(),
+            $request->content(),
+            new DateTimeImmutable($request->createdAt()),
+            $request->email(),
+            $request->ip(),
+            $request->name(),
+            new CommentStatus($request->status())
+        );
+
+        $this->commandBus->dispatch($command);
+
+        return $this->responseBuilder->redirect('admin.comments.index');
     }
 }
--- app/Infrastructure/Providers/CommentsServiceProvider.php
+++ app/Infrastructure/Providers/CommentsServiceProvider.php
@@ -4,17 +4,28 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Providers;
 
+use App\Application\Comments\Commands\CreateComment;
 use App\Application\Comments\CommentRepository;
+use App\Application\Comments\Handlers\CreateCommentHandler;
 use App\Application\Comments\ModelMapperInterface;
+use App\Application\Interfaces\CommandBusInterface;
 use App\Application\Interfaces\QueryBuilderInterface;
 use App\Domain\Comments\CommentRepositoryInterface;
+use App\Domain\Comments\Requests\AdminCommentCreateRequestInterface;
 use App\Infrastructure\Adapters\LaravelQueryBuilder;
 use App\Infrastructure\Eloquent\CommentEloquentModel;
 use App\Infrastructure\Eloquent\CommentMapper;
+use App\Infrastructure\Http\Requests\AdminCommentCreateRequest;
 use Illuminate\Support\ServiceProvider;
 
 class CommentsServiceProvider extends ServiceProvider
 {
+    public function boot(
+        CommandBusInterface $commandBus
+    ): void {
+        $commandBus->subscribe(CreateComment::class, CreateCommentHandler::class);
+    }
+
     public function register(): void
     {
         $this->app->when(CommentRepository::class)
@@ -27,5 +38,6 @@ class CommentsServiceProvider extends ServiceProvider
 
         $this->app->bind(ModelMapperInterface::class, CommentMapper::class);
         $this->app->bind(CommentRepositoryInterface::class, CommentRepository::class);
+        $this->app->bind(AdminCommentCreateRequestInterface::class, AdminCommentCreateRequest::class);
     }
 }
