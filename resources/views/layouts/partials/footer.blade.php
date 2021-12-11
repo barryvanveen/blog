3920d241af2b714b002982b1d24aaf1f62b073ca
Barry van Veen <barry+github@barryvanveen.nl>
2021-12-11 18:32:12 +0100

    responsive footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,9 @@
 @presenter(\App\Application\View\FooterPresenter)
 <footer class="w-full py-6 border-t-4 border-color">
     <div class="container-max-md md:flex md:justify-between md:items-center">
-        @include('layouts.partials.logo')
+        <div class="hidden md:block ">
+            @include('layouts.partials.logo')
+        </div>
         <nav>
             <ul class="w-full md:w-auto justify-center md:justify-end inline-flex gap-x-8">
                 @foreach($menu_items as $menu_item)
